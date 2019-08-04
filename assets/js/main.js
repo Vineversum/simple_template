92f975a09325ed2c0c370d0cbf815b70c8bb5c4f
Vineversum <Vineversum@gmail.com>
2019-08-04 21:06:03 +0300

    Кнопка меню + слайдер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
-(function ($) {
+slides = document.getElementsByClassName('carousel-slide');
+currentSlide = 0;
+
+document.querySelector('.menu__button').onclick = (e) => {
+    e.currentTarget.classList.toggle('active');
+    document.querySelector('.menu__list').classList.toggle('active');
+};
+document.querySelector('.prev').onclick = prevSlide;
+
+document.querySelector('.next').onclick = nextSlide;
 
 
+function prevSlide() {
+    slides[currentSlide].classList.toggle('active');
+    currentSlide = (--currentSlide + slides.length) % slides.length;
+    slides[currentSlide].classList.toggle('active'); 
+};
 
-})(jQuery);
+function nextSlide() {
+    slides[currentSlide].classList.toggle('active');
+    currentSlide = (++currentSlide) % slides.length;
+    slides[currentSlide].classList.toggle('active'); 
+};
+
+setInterval(nextSlide, 2000);
